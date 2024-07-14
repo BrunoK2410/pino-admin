1796cbea547fff8874225c98d34c41b31cdf4b97
Bruno <brunokatic2410@gmail.com>
2024-07-14 17:29:15 +0200

    new requests

--- src/services/apiRequests.js
+++ src/services/apiRequests.js
@@ -14,6 +14,27 @@ class ApiRequests {
     });
   }
 
+  refreshToken(refreshToken) {
+    return fetch(
+      "https://identitytoolkit.googleapis.com/v1/token?key=AIzaSyDEzVslo3fjmyLZm02vAJb0BYHznd03NPc",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          grant_type: "refresh_token",
+          refresh_token: refreshToken,
+        }),
+      }
+    ).then((response) => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      return response.json();
+    });
+  }
+
   getData(type, signal) {
     return fetch(
       `https://pino-nmpb-default-rtdb.europe-west1.firebasedatabase.app/${type}.json`,
@@ -33,13 +54,74 @@ class ApiRequests {
         for (const key in data) {
           result.push({ id: key, ...data[key] });
         }
-        const date = type === "news" ? "date" : "timestamp";
         return result.sort((a, b) => {
-          const dateA = new Date(a[date]);
-          const dateB = new Date(b[date]);
+          const dateA = new Date(a["timestamp"]);
+          const dateB = new Date(b["timestamp"]);
           return dateB - dateA;
         });
       });
   }
+
+  addNew(type, data, signal) {
+    const token = localStorage.getItem("token");
+
+    return fetch(
+      `https://pino-nmpb-default-rtdb.europe-west1.firebasedatabase.app/${type}.json?auth=` +
+        token,
+      {
+        method: "POST",
+        signal: signal,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      }
+    ).then((response) => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      return response.json();
+    });
+  }
+
+  update(type, id, data, signal) {
+    const token = localStorage.getItem("token");
+
+    return fetch(
+      `https://pino-nmpb-default-rtdb.europe-west1.firebasedatabase.app/${type}/${id}.json?auth=` +
+        token,
+      {
+        method: "PUT",
+        signal: signal,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      }
+    ).then((response) => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      return response.json();
+    });
+  }
+
+  delete(type, id) {
+    const token = localStorage.getItem("token");
+
+    return fetch(
+      `https://pino-nmpb-default-rtdb.europe-west1.firebasedatabase.app/${type}/${id}.json?auth=` +
+        token,
+      {
+        method: "DELETE",
+      }
+    ).then((response) => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      return response.json();
+    });
+  }
 }
+
 export default new ApiRequests();
